2a62e4fa485ffc63eaf8958d4d898bffda95440d
ysk8hori <ysk8.hori@gmail.com>
2021-08-23 23:00:16 +0900

    Cellsを作成。Positionをタプルに。

--- src/normal_game/cell.rs
+++ src/normal_game/cell.rs
@@ -1,4 +1,5 @@
 use crate::normal_game::setting;
+use std::rc::Rc;
 
 #[derive(Debug)]
 pub struct Cell {
@@ -6,21 +7,46 @@ pub struct Cell {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Position {
-    row: u8,
-    col: u8,
+pub struct Position(u8, u8);
+impl Position {
+    pub fn row(&self) -> u8 {
+        self.0
+    }
+    pub fn col(&self) -> u8 {
+        self.1
+    }
 }
 
-pub fn create_cells(setting: setting::GameSetting) -> Vec<Cell> {
-    let mut ret = Vec::new();
-    for row in 1..=setting.side_size() {
-        for col in 1..=setting.side_size() {
-            ret.push(Cell {
-                pos: Position { row, col },
-            });
-        }
+pub struct Cells {
+    cells: Vec<Rc<Cell>>,
+}
+
+impl Cells {
+    pub fn len(&self) -> usize {
+        self.cells.len()
     }
-    ret
+}
+
+impl Cells {
+    pub fn filter_by_row(&self, row: u8) -> Vec<Rc<Cell>> {
+        self.cells
+            .iter()
+            .filter(|c| c.pos.row() == row)
+            .map(|c| c.clone())
+            .collect()
+    }
+
+    pub fn create_cells(setting: &setting::GameSetting) -> Cells {
+        let mut cells = Vec::new();
+        for row in 1..=setting.side_size() {
+            for col in 1..=setting.side_size() {
+                cells.push(Rc::new(Cell {
+                    pos: Position(row, col),
+                }));
+            }
+        }
+        Cells { cells }
+    }
 }
 
 #[cfg(test)]
@@ -33,7 +59,7 @@ mod tests {
             #[test]
             fn returns_36_cells() {
                 assert_eq!(
-                    create_cells(setting::GameSetting {
+                    Cells::create_cells(&setting::GameSetting {
                         block_height: 2,
                         block_width: 3
                     })
@@ -44,36 +70,62 @@ mod tests {
             #[test]
             fn first_cell_position_is_1_1() {
                 assert_eq!(
-                    create_cells(setting::GameSetting {
+                    Cells::create_cells(&setting::GameSetting {
                         block_height: 2,
                         block_width: 3
-                    })[0]
+                    })
+                    .cells[0]
                         .pos,
-                    Position { row: 1, col: 1 }
+                    Position(1, 1)
                 );
             }
             #[test]
-            fn second_cell_position_is_2_1() {
+            fn second_cell_position_is_1_2() {
                 assert_eq!(
-                    create_cells(setting::GameSetting {
+                    Cells::create_cells(&setting::GameSetting {
                         block_height: 2,
                         block_width: 3
-                    })[1]
+                    })
+                    .cells[1]
                         .pos,
-                    Position { row: 1, col: 2 }
+                    Position(1, 2)
                 );
             }
             #[test]
-            fn last_cell_position_is_9_9() {
+            fn last_cell_position_is_6_6() {
                 assert_eq!(
-                    create_cells(setting::GameSetting {
+                    Cells::create_cells(&setting::GameSetting {
                         block_height: 2,
                         block_width: 3
-                    })[35]
+                    })
+                    .cells[35]
                         .pos,
-                    Position { row: 6, col: 6 }
+                    Position(6, 6)
                 );
             }
+        }
+    }
+    mod cells {
+        use super::*;
+        #[test]
+        fn test_filter_by_row() {
+            let cells_by_row = Cells::create_cells(&setting::GameSetting {
+                block_height: 2,
+                block_width: 3,
+            })
+            .filter_by_row(3);
+            let rows: Vec<&Position> = cells_by_row.iter().map(|c| &c.pos).collect();
+            assert_eq!(
+                rows,
+                [
+                    &Position(3, 1),
+                    &Position(3, 2),
+                    &Position(3, 3),
+                    &Position(3, 4),
+                    &Position(3, 5),
+                    &Position(3, 6),
+                ]
+            )
         }
     }
 }

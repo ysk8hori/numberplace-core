f521ec92068bfe3f43437fdfaa141f34ef1d4370
ysk8hori <ysk8.hori@gmail.com>
2021-08-24 23:15:29 +0900

    vertical group と horizontal group を試験

--- src/normal_game/group.rs
+++ src/normal_game/group.rs
@@ -2,19 +2,29 @@ use crate::normal_game::cell;
 use crate::normal_game::setting;
 use std::rc::Rc;
 
+#[derive(Debug)]
 pub struct Group {
-    cells: Vec<Rc<cell::Cell>>,
+    pub cells: Vec<Rc<cell::Cell>>,
 }
 
 pub fn create_groups(cells: &cell::Cells, setting: &setting::GameSetting) -> Vec<Group> {
     let vg: Vec<Group> = create_vertical_groups(&cells, &setting);
-    // let c2: Vec<Rc<cell::Cell>> = cells.iter().map(|x| x.clone()).collect();
+    let hg: Vec<Group> = create_horizontal_groups(&cells, &setting);
     vg
     // vec![vg, Group { cells: c2 }]
 }
 
 fn create_vertical_groups(cells: &cell::Cells, setting: &setting::GameSetting) -> Vec<Group> {
-    let rows: Vec<u8> = (0..setting.side_size()).collect();
+    let cols: Vec<u8> = (1..=setting.side_size()).collect();
+    cols.iter()
+        .map(|n| Group {
+            cells: cells.filter_by_column(*n),
+        })
+        .collect()
+}
+
+fn create_horizontal_groups(cells: &cell::Cells, setting: &setting::GameSetting) -> Vec<Group> {
+    let rows: Vec<u8> = (1..=setting.side_size()).collect();
     rows.iter()
         .map(|n| Group {
             cells: cells.filter_by_row(*n),
@@ -22,13 +32,13 @@ fn create_vertical_groups(cells: &cell::Cells, setting: &setting::GameSetting) -
         .collect()
 }
 
+// fn create_block_groups(cells: &cell::Cells, setting: &setting::GameSetting) -> Vec<Group> {
+//     let block_rows_list: Vec<u8> = ()
+// }
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn create_groups_test() {
-        assert_eq!(true, true);
-    }
     #[test]
     fn test_create_vertical_groups() {
         let setting = setting::GameSetting {
@@ -37,5 +47,24 @@ mod tests {
         };
         let vg = create_vertical_groups(&cell::Cells::create_cells(&setting), &setting);
         assert_eq!(vg.len(), 6);
+        assert_eq!(vg[0].cells.len(), 6);
+        assert_eq!(*vg[0].cells[0].pos(), cell::Position::new(1, 1));
+        assert_eq!(*vg[0].cells[5].pos(), cell::Position::new(6, 1));
+        assert_eq!(*vg[5].cells[0].pos(), cell::Position::new(1, 6));
+        assert_eq!(*vg[5].cells[5].pos(), cell::Position::new(6, 6));
+    }
+    #[test]
+    fn test_create_horizontal_groups() {
+        let setting = setting::GameSetting {
+            block_height: 2,
+            block_width: 3,
+        };
+        let hg = create_horizontal_groups(&cell::Cells::create_cells(&setting), &setting);
+        assert_eq!(hg.len(), 6);
+        assert_eq!(hg[0].cells.len(), 6);
+        assert_eq!(*hg[0].cells[0].pos(), cell::Position::new(1, 1));
+        assert_eq!(*hg[0].cells[5].pos(), cell::Position::new(1, 6));
+        assert_eq!(*hg[5].cells[0].pos(), cell::Position::new(6, 1));
+        assert_eq!(*hg[5].cells[5].pos(), cell::Position::new(6, 6));
     }
 }

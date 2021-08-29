32373665617984f31c0d40f890382955f8ec6443
ysk8hori <ysk8.hori@gmail.com>
2021-08-29 23:24:37 +0900

    Implemented load issue.

--- src/normal_game.rs
+++ src/normal_game.rs
@@ -20,6 +20,26 @@ impl NormalGame {
             groups,
         }
     }
+    /// ' 7     6 |6   1   3|  54 87  |  8   4  | 1  3  5 |  9   1  |  35 12  |7   2   8| 5     9 '
+    fn load(issue: &str, setting: setting::GameSetting) -> NormalGame {
+        let game = NormalGame::new(setting);
+        let answer_columns: Vec<&str> = issue.split("|").collect();
+        for (y, horizontal_line) in answer_columns.iter().enumerate() {
+            let chars: Vec<char> = horizontal_line.chars().collect();
+            for (x, answer) in chars.iter().enumerate() {
+                if *answer == ' ' {
+                    continue;
+                }
+                let answer: u8 = String::from(*answer).parse().expect("issue is wrong.");
+                game.cells
+                    .find_by_position(&cell::Position::new(x as u8, y as u8))
+                    .unwrap()
+                    .borrow_mut()
+                    .set_answer(answer);
+            }
+        }
+        game
+    }
 }
 
 #[cfg(test)]
@@ -80,6 +100,71 @@ mod tests {
                 block_width: 3,
             });
             assert_eq!(game.groups.len(), 27);
+        }
+    }
+    mod test_load {
+        use super::*;
+        fn game() -> NormalGame {
+            NormalGame::load(" 7     6 |6   1   3|  54 87  |  8   4  | 1  3  5 |  9   1  |  35 12  |7   2   8| 5     9 ", setting::GameSetting {
+                block_height: 3,
+                block_width: 3,
+            })
+        }
+        mod load_9_9 {
+            use super::*;
+            #[test]
+            fn has_81_cells() {
+                assert_eq!(game().cells.len(), 81);
+            }
+
+            #[test]
+            fn none_0_0() {
+                assert_eq!(
+                    game()
+                        .cells
+                        .find_by_position(&cell::Position::new(0, 0))
+                        .unwrap()
+                        .borrow()
+                        .answer(),
+                    None
+                );
+            }
+            #[test]
+            fn some_1_0() {
+                assert_eq!(
+                    game()
+                        .cells
+                        .find_by_position(&cell::Position::new(1, 0))
+                        .unwrap()
+                        .borrow()
+                        .answer(),
+                    Some(7)
+                );
+            }
+            #[test]
+            fn some_2_0() {
+                assert_eq!(
+                    game()
+                        .cells
+                        .find_by_position(&cell::Position::new(2, 0))
+                        .unwrap()
+                        .borrow()
+                        .answer(),
+                    None
+                );
+            }
+            #[test]
+            fn some_7_0() {
+                assert_eq!(
+                    game()
+                        .cells
+                        .find_by_position(&cell::Position::new(7, 0))
+                        .unwrap()
+                        .borrow()
+                        .answer(),
+                    Some(6)
+                );
+            }
         }
     }
 }
